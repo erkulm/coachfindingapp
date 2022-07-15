c76c4839f00edc4963c34b3813f48f6769fe63c4
Mahmut Erkul <mahmut.erkul@kafein.com.tr>
2022-07-15 16:08:20 +0300

    base ui components added

--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import Router from './router.js';
-import Store from './store/index.js'
+import Store from './store/index.js';
+import BaseCard from './components/ui/BaseCard.vue';
+import BaseButton from './components/ui/BaseButton.vue';
+import BaseBadge from './components/ui/BaseBadge.vue';
+
+
 
 const app =createApp(App);
 
 app.use(Router);
 app.use(Store);
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-badge', BaseBadge);
+
+
 
 app.mount('#app');
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -3,16 +3,18 @@
         FILTER
     </section>
     <section>
-        <div class="controls">
-            <button>Refresh</button>
-            <router-link to="/register">Register as Coach</router-link>
+        <base-card>
+            <div class="controls">
+                <base-button mode="outline">Refresh</base-button>
+                <base-button to="/register">Register as Coach</base-button>
 
-        </div>
-        <ul v-if="hasCoaches">
-            <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :firstName="coach.firstName"
-                :lastName="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
-        </ul>
-        <h3 v-else> No coaches Found</h3>
+            </div>
+            <ul v-if="hasCoaches">
+                <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :firstName="coach.firstName"
+                    :lastName="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
+            </ul>
+            <h3 v-else> No coaches Found</h3>
+        </base-card>
     </section>
 </template>
 
