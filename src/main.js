a552277425058db93a71878059d4b590a521c1a8
Mahmut Erkul <mahmut.erkul@kafein.com.tr>
2022-07-15 14:39:36 +0300

    empty components for pages & routes registered

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 import { createApp } from 'vue';
+import App from './App.vue';
+import Router from './router.js';
 
-createApp({}).mount('#app');
+const app =createApp(App);
+
+app.use(Router);
+
+app.mount('#app');
