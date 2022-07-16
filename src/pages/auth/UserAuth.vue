47b4a09081fd95f36b29ae2c0851df854f1c1e0a
mahmut <erkul@itu.edu.tr>
2022-07-17 01:48:33 +0300

    auth page

---
+++ src/pages/auth/UserAuth.vue
@@ -0,0 +1,79 @@
+<template>
+    <section>
+        <base-card>
+            <form>
+                <div class="form-control">
+                    <label for="email">Email</label>
+                    <input type="email" id="email" />
+                </div>
+                <div class="form-control">
+                    <label for="password">Password</label>
+                    <input type="password" id="password" />
+                </div>
+                <base-button>Login</base-button>
+                <base-button type="button" mode="flat">Sign up instead</base-button>
+            </form>
+        </base-card>
+    </section>
+</template>
+
+<style scoped>
+.form-control {
+    margin: 0.5rem 0;
+}
+
+label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+}
+
+input[type='checkbox']+label {
+    font-weight: normal;
+    display: inline;
+    margin: 0 0 0 0.5rem;
+}
+
+input,
+textarea {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+}
+
+input:focus,
+textarea:focus {
+    background-color: #f0e6fd;
+    outline: none;
+    border-color: #3d008d;
+}
+
+input[type='checkbox'] {
+    display: inline;
+    width: auto;
+    border: none;
+}
+
+input[type='checkbox']:focus {
+    outline: #3d008d solid 1px;
+}
+
+h3 {
+    margin: 0.5rem 0;
+    font-size: 1rem;
+}
+
+.invalid label {
+    color: red;
+}
+
+input.invalid,
+textarea.invalid {
+    border: 1px solid red;
+}
+
+.danger {
+    color: red;
+}
+</style>
